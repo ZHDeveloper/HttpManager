731fae3ac6278d3734def7174b4bf8ffd5171946
ZhiHua <zhcoder2011@gmail.com>
2016-06-01 08:51:08 +0800

    Initial Commit

---
+++ AFNetworkingTests/AFNetworkingTests.m
@@ -0,0 +1,39 @@
+//
+//  AFNetworkingTests.m
+//  AFNetworkingTests
+//
+//  Created by AdminZhiHua on 16/6/1.
+//  Copyright © 2016年 AdminZhiHua. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFNetworkingTests : XCTestCase
+
+@end
+
+@implementation AFNetworkingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
